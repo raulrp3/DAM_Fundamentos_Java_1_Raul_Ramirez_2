20b8c0a934990a96321b606c70978708b08e6ba2
raulrp3 <raul.ramirez@escuelaestech.es>
2018-10-19 15:55:07 +0200

    Mejorando funciones

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,14 +3,37 @@ package com.company;
 import java.util.Scanner;
 
 public class Main {
-    public static double validarRadio(Scanner sc){
-        double radioValidado;
-        while (!sc.hasNextDouble()){
-            System.out.print("¡Introduce un valor numérico para el radio! ");
-            sc.next();
+    public static double validador(int opcion){
+        double validado = 0;
+        Scanner sc = new Scanner(System.in);
+        switch (opcion){
+            case  0:{
+                while(!sc.hasNextInt()){
+                    System.out.print("¡Por favor introduce un número entero! ");
+                    sc.next();
+                }
+                validado = sc.nextDouble();
+            }
+            break;
+            case 1:{
+                while(!sc.hasNextDouble()){
+                    System.out.print("¡Por favor introduce un número decimal! ");
+                    sc.next();
+                }
+                validado = sc.nextDouble();
+            }
+            break;
         }
-        radioValidado = sc.nextDouble();
-        return radioValidado;
+        return  validado;
+    }
+    public static boolean validarRadio(double radio){
+        boolean validado = false;
+        while (radio < 0){
+            System.out.print("El valor del radio debe ser positivo.Introduce otro valor: ");
+            radio = validador(1);
+            validado = true;
+        }
+        return validado;
     }
     public static double calcularArea(double radio){
         return Math.PI * Math.pow(radio, 2);
@@ -20,13 +43,11 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.println("¡Hola!,vamos a calcular el área de una circunferencia.");
         System.out.print("Introduce el radio: ");
-        radio = validarRadio(sc);
-        while (radio < 0){
-            System.out.print("El valor del radio debe ser positivo.Introduce otro valor: ");
-            radio = validarRadio(sc);
+        radio = validador(1);
+        if (validarRadio(radio)){
+            sc.close();
+            area = calcularArea(radio);
+            System.out.printf("El área de la circunferencia es: %f\n", area);
         }
-        sc.close();
-        area = calcularArea(radio);
-        System.out.printf("El área de la circunferencia es: %f\n", area);
     }
 }
